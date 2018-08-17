310fdc05969759740923de1d40ed4c76d23a70a6
Bjarte Aune Olsen <bjarte@basementmedia.no>
2018-08-18 00:22:34 +0200

    Added Norwegian translation

--- src/DNTCaptcha.Core/Providers/HumanReadableIntegerProvider.cs
+++ src/DNTCaptcha.Core/Providers/HumanReadableIntegerProvider.cs
@@ -1,138 +1,157 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using DNTCaptcha.Core.Contracts;
 
 namespace DNTCaptcha.Core.Providers
 {
-	/// <summary>
-	/// Convert a number into words
-	/// </summary>
-	public class HumanReadableIntegerProvider : IHumanReadableIntegerProvider
-	{
-		private readonly IDictionary<Language, string> _and = new Dictionary<Language, string>
-		{
-			{ Language.English, " " },
-			{ Language.Persian, " و " }
-		};
-		private readonly IList<NumberWord> _numberWords = new List<NumberWord>
-		{
-			new NumberWord { Group= DigitGroup.Ones, Language= Language.English, Names=
-				new List<string> { string.Empty, "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" }},
-			new NumberWord { Group= DigitGroup.Ones, Language= Language.Persian, Names=
-				new List<string> { string.Empty, "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه" }},
+    /// <summary>
+    /// Convert a number into words
+    /// </summary>
+    public class HumanReadableIntegerProvider : IHumanReadableIntegerProvider
+    {
+        private readonly IDictionary<Language, string> _and = new Dictionary<Language, string>
+        {
+            { Language.English, " " },
+            { Language.Persian, " و " },
+            { Language.Norwegian, " og " }
+        };
+        private readonly IList<NumberWord> _numberWords = new List<NumberWord>
+        {
+            new NumberWord { Group= DigitGroup.Ones, Language= Language.English, Names=
+                new List<string> { string.Empty, "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" }},
+            new NumberWord { Group= DigitGroup.Ones, Language= Language.Persian, Names=
+                new List<string> { string.Empty, "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه" }},
+            new NumberWord { Group= DigitGroup.Ones, Language= Language.Norwegian, Names=
+                new List<string> { string.Empty, "en", "to", "tre", "fire", "fem", "seks", "syv", "åtte", "ni" }},
 
-			new NumberWord { Group= DigitGroup.Teens, Language= Language.English, Names=
-				new List<string> { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" }},
-			new NumberWord { Group= DigitGroup.Teens, Language= Language.Persian, Names=
-				new List<string> { "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده" }},
+            new NumberWord { Group= DigitGroup.Teens, Language= Language.English, Names=
+                new List<string> { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" }},
+            new NumberWord { Group= DigitGroup.Teens, Language= Language.Persian, Names=
+                new List<string> { "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده" }},
+            new NumberWord { Group= DigitGroup.Teens, Language= Language.Norwegian, Names=
+                new List<string> { "ti", "elleve", "tolv", "tretten", "fjorten", "femten", "seksten", "sytten", "atten", "nitten" }},
 
-			new NumberWord { Group= DigitGroup.Tens, Language= Language.English, Names=
-				new List<string> { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" }},
-			new NumberWord { Group= DigitGroup.Tens, Language= Language.Persian, Names=
-				new List<string> { "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" }},
+            new NumberWord { Group= DigitGroup.Tens, Language= Language.English, Names=
+                new List<string> { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" }},
+            new NumberWord { Group= DigitGroup.Tens, Language= Language.Persian, Names=
+                new List<string> { "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" }},
+            new NumberWord { Group= DigitGroup.Tens, Language= Language.Norwegian, Names=
+                new List<string> { "tjue", "tretti", "førti", "femti", "seksti", "sytti", "åtti", "nitti" }},
 
-			new NumberWord { Group= DigitGroup.Hundreds, Language= Language.English, Names=
-				new List<string> {string.Empty, "One Hundred", "Two Hundred", "Three Hundred", "Four Hundred",
-					"Five Hundred", "Six Hundred", "Seven Hundred", "Eight Hundred", "Nine Hundred" }},
-			new NumberWord { Group= DigitGroup.Hundreds, Language= Language.Persian, Names=
-				new List<string> {string.Empty, "یکصد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد" , "نهصد" }},
+            new NumberWord { Group= DigitGroup.Hundreds, Language= Language.English, Names=
+                new List<string> {string.Empty, "One Hundred", "Two Hundred", "Three Hundred", "Four Hundred",
+                    "Five Hundred", "Six Hundred", "Seven Hundred", "Eight Hundred", "Nine Hundred" }},
+            new NumberWord { Group= DigitGroup.Hundreds, Language= Language.Persian, Names=
+                new List<string> {string.Empty, "یکصد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد" , "نهصد" }},
+            new NumberWord { Group= DigitGroup.Hundreds, Language= Language.Norwegian, Names=
+                new List<string> {string.Empty, "ett hundre", "to hundre", "tre hundre", "fire hundre",
+                    "fem hundre", "seks hundre", "syv hundre", "åtte hundre", "ni hundre" }},
 
-			new NumberWord { Group= DigitGroup.Thousands, Language= Language.English, Names=
-			  new List<string> { string.Empty, " Thousand", " Million", " Billion"," Trillion", " Quadrillion", " Quintillion", " Sextillian",
-			" Septillion", " Octillion", " Nonillion", " Decillion", " Undecillion", " Duodecillion", " Tredecillion",
-			" Quattuordecillion", " Quindecillion", " Sexdecillion", " Septendecillion", " Octodecillion", " Novemdecillion",
-			" Vigintillion", " Unvigintillion", " Duovigintillion", " 10^72", " 10^75", " 10^78", " 10^81", " 10^84", " 10^87",
-			" Vigintinonillion", " 10^93", " 10^96", " Duotrigintillion", " Trestrigintillion" }},
-			new NumberWord { Group= DigitGroup.Thousands, Language= Language.Persian, Names=
-			  new List<string> { string.Empty, " هزار", " میلیون", " میلیارد"," تریلیون", " Quadrillion", " Quintillion", " Sextillian",
-			" Septillion", " Octillion", " Nonillion", " Decillion", " Undecillion", " Duodecillion", " Tredecillion",
-			" Quattuordecillion", " Quindecillion", " Sexdecillion", " Septendecillion", " Octodecillion", " Novemdecillion",
-			" Vigintillion", " Unvigintillion", " Duovigintillion", " 10^72", " 10^75", " 10^78", " 10^81", " 10^84", " 10^87",
-			" Vigintinonillion", " 10^93", " 10^96", " Duotrigintillion", " Trestrigintillion" }},
-		};
-		private readonly IDictionary<Language, string> _negative = new Dictionary<Language, string>
-		{
-			{ Language.English, "Negative " },
-			{ Language.Persian, "منهای " }
-		};
-		private readonly IDictionary<Language, string> _zero = new Dictionary<Language, string>
-		{
-			{ Language.English, "Zero" },
-			{ Language.Persian, "صفر" }
-		};
+            new NumberWord { Group= DigitGroup.Thousands, Language= Language.English, Names=
+              new List<string> { string.Empty, " Thousand", " Million", " Billion"," Trillion", " Quadrillion", " Quintillion", " Sextillian",
+            " Septillion", " Octillion", " Nonillion", " Decillion", " Undecillion", " Duodecillion", " Tredecillion",
+            " Quattuordecillion", " Quindecillion", " Sexdecillion", " Septendecillion", " Octodecillion", " Novemdecillion",
+            " Vigintillion", " Unvigintillion", " Duovigintillion", " 10^72", " 10^75", " 10^78", " 10^81", " 10^84", " 10^87",
+            " Vigintinonillion", " 10^93", " 10^96", " Duotrigintillion", " Trestrigintillion" }},
+            new NumberWord { Group= DigitGroup.Thousands, Language= Language.Persian, Names=
+              new List<string> { string.Empty, " هزار", " میلیون", " میلیارد"," تریلیون", " Quadrillion", " Quintillion", " Sextillian",
+            " Septillion", " Octillion", " Nonillion", " Decillion", " Undecillion", " Duodecillion", " Tredecillion",
+            " Quattuordecillion", " Quindecillion", " Sexdecillion", " Septendecillion", " Octodecillion", " Novemdecillion",
+            " Vigintillion", " Unvigintillion", " Duovigintillion", " 10^72", " 10^75", " 10^78", " 10^81", " 10^84", " 10^87",
+            " Vigintinonillion", " 10^93", " 10^96", " Duotrigintillion", " Trestrigintillion" }},
+            new NumberWord { Group= DigitGroup.Thousands, Language= Language.Norwegian, Names=
+              new List<string> { string.Empty, " tusen", " million", " milliard"," billion", " billiard", " trillion", " trilliard",
+            " kvadrillion", " kvintillion", " sekstillion", " septillion", " oktillion", " nonillion", " desillion",
+            // Not translated the next
+            " Quattuordecillion", " Quindecillion", " Sexdecillion", " Septendecillion", " Octodecillion", " Novemdecillion",
+            " Vigintillion", " Unvigintillion", " Duovigintillion", " 10^72", " 10^75", " 10^78", " 10^81", " 10^84", " 10^87",
+            " Vigintinonillion", " 10^93", " 10^96", " Duotrigintillion", " Trestrigintillion" }},
+        };
+        private readonly IDictionary<Language, string> _negative = new Dictionary<Language, string>
+        {
+            { Language.English, "Negative " },
+            { Language.Persian, "منهای " },
+            { Language.Norwegian, "Negativ" }
+        };
+        private readonly IDictionary<Language, string> _zero = new Dictionary<Language, string>
+        {
+            { Language.English, "Zero" },
+            { Language.Persian, "صفر" },
+            { Language.Norwegian, "Null" }
+        };
 
-		// Public Methods (5)
+        // Public Methods (5)
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(int number, Language language)
-		{
-			return NumberToText((long)number, language);
-		}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(int number, Language language)
+        {
+            return NumberToText((long)number, language);
+        }
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(uint number, Language language)
-		{
-			return NumberToText((long)number, language);
-		}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(uint number, Language language)
+        {
+            return NumberToText((long)number, language);
+        }
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(byte number, Language language)
-		{
-			return NumberToText((long)number, language);
-		}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(byte number, Language language)
+        {
+            return NumberToText((long)number, language);
+        }
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(decimal number, Language language)
-		{
-			return NumberToText((long)number, language);
-		}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(decimal number, Language language)
+        {
+            return NumberToText((long)number, language);
+        }
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(double number, Language language)
-		{
-			return NumberToText((long)number, language);
-		}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(double number, Language language)
+        {
+            return NumberToText((long)number, language);
+        }
 
-		/// <summary>
-		/// display a numeric value using the equivalent text
-		/// </summary>
-		/// <param name="number">input number</param>
-		/// <param name="language">local language</param>
-		/// <returns>the equivalent text</returns>
-		public string NumberToText(long number, Language language)
-		{
-			if (number == 0)
-			{
-				return _zero[language];
-			}
+        /// <summary>
+        /// display a numeric value using the equivalent text
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="language">local language</param>
+        /// <returns>the equivalent text</returns>
+        public string NumberToText(long number, Language language)
+        {
+            if (number == 0)
+            {
+                return _zero[language];
+            }
 
-			if (number < 0)
-			{
+            if (number < 0)
+            {
 				return _negative[language] + NumberToText(-number, language);
 			}
 
@@ -156,7 +175,7 @@ namespace DNTCaptcha.Core.Providers
 			if (wordValue.Length > 0)
 			{
 				wordValue += _and[language];
-			}
+            }
 
 			if (number < 10)
 			{
