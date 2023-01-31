156aa9557d57d00fc964a0185cc5ead2903bb8c9
Mohsen Rajabi <m.kabir8895@gmail.com>
2023-01-31 10:27:01 +0330

    Update NumbersNormalizer.cs
    improvement number to english

--- src/DNTCaptcha.Core/NumbersNormalizer.cs
+++ src/DNTCaptcha.Core/NumbersNormalizer.cs
@@ -1,5 +1,3 @@
-﻿using System;
-
 namespace DNTCaptcha.Core;
 
 internal static class NumbersNormalizer
@@ -10,32 +8,68 @@ internal static class NumbersNormalizer
     /// <param name="data">Persian number</param>
     internal static string ToEnglishNumbers(this string data)
     {
-        if (string.IsNullOrWhiteSpace(data))
-        {
-            return string.Empty;
-        }
+        if (string.IsNullOrWhiteSpace(data)) return string.Empty;
 
-        return
-            data.Replace("\u0660", "0", StringComparison.Ordinal) //٠
-                .Replace("\u06F0", "0", StringComparison.Ordinal) //۰
-                .Replace("\u0661", "1", StringComparison.Ordinal) //١
-                .Replace("\u06F1", "1", StringComparison.Ordinal) //۱
-                .Replace("\u0662", "2", StringComparison.Ordinal) //٢
-                .Replace("\u06F2", "2", StringComparison.Ordinal) //۲
-                .Replace("\u0663", "3", StringComparison.Ordinal) //٣
-                .Replace("\u06F3", "3", StringComparison.Ordinal) //۳
-                .Replace("\u0664", "4", StringComparison.Ordinal) //٤
-                .Replace("\u06F4", "4", StringComparison.Ordinal) //۴
-                .Replace("\u0665", "5", StringComparison.Ordinal) //٥
-                .Replace("\u06F5", "5", StringComparison.Ordinal) //۵
-                .Replace("\u0666", "6", StringComparison.Ordinal) //٦
-                .Replace("\u06F6", "6", StringComparison.Ordinal) //۶
-                .Replace("\u0667", "7", StringComparison.Ordinal) //٧
-                .Replace("\u06F7", "7", StringComparison.Ordinal) //۷
-                .Replace("\u0668", "8", StringComparison.Ordinal) //٨
-                .Replace("\u06F8", "8", StringComparison.Ordinal) //۸
-                .Replace("\u0669", "9", StringComparison.Ordinal) //٩
-                .Replace("\u06F9", "9", StringComparison.Ordinal) //۹
-            ;
+        var length = data.Length;
+        return string.Create(length, data, (chars, context) =>
+        {
+            for (var i = 0; i < length; i++)
+                switch (context[i])
+                {
+                    case '۰':
+                    case '٠':
+                        chars[i] = '0';
+                        break;
+
+                    case '١':
+                    case '۱':
+                        chars[i] = '1';
+                        break;
+
+                    case '٢':
+                    case '۲':
+                        chars[i] = '2';
+                        break;
+
+                    case '۳':
+                    case '٣':
+                        chars[i] = '3';
+                        break;
+
+                    case '۴':
+                    case '٤':
+                        chars[i] = '4';
+                        break;
+
+                    case '۵':
+                    case '٥':
+                        chars[i] = '5';
+                        break;
+
+                    case '۶':
+                    case '٦':
+                        chars[i] = '6';
+                        break;
+
+                    case '۷':
+                    case '٧':
+                        chars[i] = '7';
+                        break;
+
+                    case '۸':
+                    case '٨':
+                        chars[i] = '8';
+                        break;
+
+                    case '۹':
+                    case '٩':
+                        chars[i] = '9';
+                        break;
+
+                    default:
+                        chars[i] = context[i];
+                        break;
+                }
+        });
     }
 }
