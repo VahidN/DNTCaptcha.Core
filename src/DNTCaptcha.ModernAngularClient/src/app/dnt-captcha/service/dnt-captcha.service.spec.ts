60635b26e71163ddde032707e7af1a10acbcb4dd
Mateusz Halada <haladamateusz@gmail.com>
2023-09-22 10:56:21 +0200

    add http tests for login and dnt-captcha services

--- src/DNTCaptcha.ModernAngularClient/src/app/dnt-captcha/service/dnt-captcha.service.spec.ts
+++ src/DNTCaptcha.ModernAngularClient/src/app/dnt-captcha/service/dnt-captcha.service.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { DntCaptchaService } from './dnt-captcha.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { first } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { DntCaptchaParams } from '../interfaces/dnt-captcha-params.interface';
+
+const captchaDataMock: DntCaptchaParams = {
+  dntCaptchaImgUrl: 'dntCaptchaImgUrl',
+  dntCaptchaTextValue: 'dntCaptchaTextValue',
+  dntCaptchaTokenValue: 'dntCaptchaTokenValue',
+  dntCaptchaId: 'dntCaptchaId'
+};
 
 describe('DntCaptchaService', () => {
   let service: DntCaptchaService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,10 +29,36 @@ describe('DntCaptchaService', () => {
         }
       ]
     });
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(DntCaptchaService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should perform GET operation and return captchaData', () => {
+    let captchaData;
+
+    service
+      .getDntCaptchaParams()
+      .pipe(first())
+      .subscribe((data) => {
+        captchaData = data;
+      });
+    const req = httpMock.expectOne('api/account/CreateDNTCaptchaParams');
+    req.flush(captchaDataMock);
+
+    expect(req.request.method).toEqual('GET');
+    expect(captchaData).toEqual({
+      dntCaptchaImgUrl: 'dntCaptchaImgUrl',
+      dntCaptchaTextValue: 'dntCaptchaTextValue',
+      dntCaptchaTokenValue: 'dntCaptchaTokenValue',
+      dntCaptchaId: 'dntCaptchaId'
+    });
   });
 });
